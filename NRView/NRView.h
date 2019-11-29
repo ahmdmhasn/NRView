4a952d1bfa6a384c7f986a24adcf40c329911b4f
Ahmed M. Hassan <ahmdmhasn@gmail.com>
2019-11-29 15:03:51 -0800

    Initial Commit

---
+++ NRView/NRView.h
@@ -0,0 +1,19 @@
+//
+//  NRView.h
+//  NRView
+//
+//  Created by Ahmed M. Hassan on 11/29/19.
+//  Copyright © 2019 Ahmed M. Hassan. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NRView.
+FOUNDATION_EXPORT double NRViewVersionNumber;
+
+//! Project version string for NRView.
+FOUNDATION_EXPORT const unsigned char NRViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NRView/PublicHeader.h>
+
+
